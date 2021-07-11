a874d7cb07dfbffa20995d11507d20dba34d4420
amanpatniajmer <amanpatniajmer@gmail.com>
2021-07-11 13:50:22 +0530

    modals UI, chats UI, code refactored, removeParticipant, InviteParticipant

--- src/messaging.js
+++ src/messaging.js
@@ -1,29 +1,5 @@
 function updateAddParticipants(id, participants) {
     let contentAll = '';
-    console.log(participants)
-    for (let i = 0; i < participants.length; i++) {
-        let content = "<div class='contact-card'>\
-            <div class='upper'>\
-                <div>\
-                    <div class='avatar'>" +
-            getAvatar(participants[i].id) +
-            "</div>\
-                    <span class='name'>" +
-            nickNames[participants[i].id] +
-            "</span>\
-                </div>\
-                <div>\
-               </div>\
-            </div>\
-        </div>";
-        contentAll += content;
-    }
-    document.getElementById(id).innerHTML = contentAll;
-}
-
-function updateParticipants(participants) {
-    let contentAll = '';
-    let id = 'participantsList';
     for (let i = 0; i < participants.length; i++) {
         let content = "<div class='contact-card'>\
             <div class='upper'>\
@@ -35,10 +11,10 @@ function updateParticipants(participants) {
             participants[i].name +
             "</span>\
                 </div></div>\
-                <div class='lower'>\
-                    <button onclick='removeParticipant(\"" +
-            participants[i].id +
-            "\")'>Remove <i class='fa fa-close'></i> </button>\
+               <div class='lower'>\
+                    <button onclick='sendInvite(\"" +
+            participants[i].uid +
+            "\")'>Invite <i class='fa fa-plus'></i> </button>\
                </div>\
             </div>\
         </div>";
@@ -47,25 +23,35 @@ function updateParticipants(participants) {
     document.getElementById(id).innerHTML = contentAll;
 }
 
-function updateRoomsList(rooms, id) {
-    let contentAll = "";
-    for (let i = 0; i < rooms.length; i++) {
-        let content = "<div class='contact-card' style='cursor:pointer' onclick=\"setRemoteChat('"+rooms[i].id+"', '"+rooms[i].name+"')\">\
-                    <div class='upper'>\
-                    <div class='avatar' style=\"background-color:" +
-            getAvatarColor(rooms[i].id) +
-            "\">" +
-            getAvatar(rooms[i].name) +
-            "</div>\
-                            <div class='name'>" +
-            rooms[i].name +
-            "</div>\
-                        </div>\
-                        <div class='lower'>\
-                            <div></div>\
-                        </div>\
-                </div>";
-        contentAll += content;
-    }
-    document.getElementById(id).innerHTML = contentAll;
+function createParticipantCard(participant) {
+        let content = `<div id="${"participant"+participant.docID}" class="contact-card">
+            <div class='upper'>
+                    <div class='avatar'>
+            ${getAvatar(nickNames[participant.id])}
+            </div>
+                    <span class='name'>
+            ${nickNames[participant.id]}
+            </span>
+                </div>
+                <div class='lower'>
+                    ${localStorage.getItem('uid')!==participant.id?`<button onclick='removeParticipant("${participant.id}", "${participant.docID}")'>Remove <i class='fa fa-close'></i> </button>`:""}
+               </div>
+            </div>`;
+    return content;
+}
+
+function createRoomCard(room) {
+        let content = `<div class='contact-card' style='cursor:pointer' onclick="setRemoteChat('${room.id}', '${room.name}')">
+                    <div class='upper'>
+                    <div class='avatar' style="background-color:
+            ${getAvatarColor(room.id)}">
+            ${getAvatar(room.name)}</div>
+                    <div class='name'> ${room.name}</div>
+                        </div>
+                        <div class='lower'>
+                            <div></div>
+                        </div>
+                </div>`;
+        
+    return content;
 }
